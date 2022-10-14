b76532b388796607243805c4389a3ff039f63287
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-14 09:27:23 -0300

    Adicionando nome do user

--- components/Conteudo/index.tsx
+++ components/Conteudo/index.tsx
@@ -1,14 +1,21 @@
+import React from 'react'
 import { ReactNode } from 'react'
 import AnimateFadeDiv from '../../motion/AnimateFadeDiv'
+import { UserLoggedContext } from '../../context/UserLoggedContext'
 
 interface ConteudoProps {
 	children: ReactNode
 }
 
 function Conteudo(props: ConteudoProps) {
+	const { user } = React.useContext(UserLoggedContext)
+
 	return (
-		<section className='w-full p-5 bg-gray-300 text-black dark:text-white dark:bg-gray-700'>
+		<section className='flex justify-between w-full p-5 bg-gray-300 text-black dark:text-white dark:bg-gray-700'>
 			<AnimateFadeDiv>{props.children}</AnimateFadeDiv>
+			{user?.email && (
+				<h2 className='hidden sm:block'>Bem vindo(a) {user?.displayName ? user.displayName : user.email}</h2>
+			)}
 		</section>
 	)
 }
