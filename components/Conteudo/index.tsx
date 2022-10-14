f8376b9a17d94d7c014459931f53f9df84e2c395
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-14 09:54:15 -0300

    Modificando estilos do container user

--- components/Conteudo/index.tsx
+++ components/Conteudo/index.tsx
@@ -14,7 +14,7 @@ function Conteudo(props: ConteudoProps) {
 		<section className='flex justify-between w-full p-5 bg-gray-300 text-black dark:text-white dark:bg-gray-700'>
 			<AnimateFadeDiv>{props.children}</AnimateFadeDiv>
 			{user?.email && (
-				<div className='flex gap-2'>
+				<div className='flex items-center gap-2 h-fit'>
 					<h2 className='hidden sm:block'>
 						Bem vindo(a) {user?.displayName ? user.displayName : user.email}
 					</h2>
