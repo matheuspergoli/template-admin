2fb37d5d854124ed700517f7b0216aeb7a1e601b
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-14 06:11:31 -0300

    Adicionando classe para rota ativa

--- components/Sidebar/MenuItem.tsx
+++ components/Sidebar/MenuItem.tsx
@@ -5,12 +5,14 @@ interface MenuItemProps {
 	url: string
 	texto: string
 	icone: any
+	className: string
 }
 
 function MenuItem(props: MenuItemProps) {
 	return (
 		<Link href={props.url} passHref>
-			<li className='w-20 h-16 p-2 flex flex-col items-center cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700 dark:text-white'>
+			<li
+				className={`w-20 h-16 p-2 flex flex-col items-center cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700 dark:text-white ${props.className}`}>
 				{props.icone}
 				<span className='text-xs'>{props.texto}</span>
 			</li>
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -3,15 +3,33 @@ import ButtonDarkMode from './ButtonDarkMode'
 import Logo from './Logo'
 import MenuItem from './MenuItem'
 import MenuItemLogout from './MenuItemLogout'
+import { useRouter } from 'next/router'
 
 function Sidebar() {
+	const router = useRouter()
+
 	return (
 		<aside className='flex flex-col w-20 h-screen bg-white dark:bg-gray-900'>
 			<Logo />
 			<ul className='flex flex-col gap-2 flex-grow'>
-				<MenuItem url='/' icone={IconeHome(6)} texto='Início' />
-				<MenuItem url='/ajustes' icone={IconeAjustes(6)} texto='Ajustes' />
-				<MenuItem url='/notificacoes' icone={IconeSino(6)} texto='Notificações' />
+				<MenuItem
+					url='/'
+					icone={IconeHome(6)}
+					texto='Início'
+					className={`${router.pathname === '/' ? 'text-blue-600 dark:text-blue-600' : ''}`}
+				/>
+				<MenuItem
+					url='/ajustes'
+					icone={IconeAjustes(6)}
+					texto='Ajustes'
+					className={`${router.pathname === '/ajustes' ? 'text-blue-600 dark:text-blue-600' : ''}`}
+				/>
+				<MenuItem
+					url='/notificacoes'
+					icone={IconeSino(6)}
+					texto='Notificações'
+					className={`${router.pathname === '/notificacoes' ? 'text-blue-600 dark:text-blue-600' : ''}`}
+				/>
 				<ButtonDarkMode />
 			</ul>
 			<ul>
