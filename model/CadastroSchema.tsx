b81e798679f92d72d1c92616730ceeb8553558c0
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-13 21:01:56 -0300

    Modificando mensagem de erro

--- model/CadastroSchema.tsx
+++ model/CadastroSchema.tsx
@@ -1,6 +1,6 @@
 import * as Yup from 'yup'
 
 export const CadastroSchema = Yup.object().shape({
-	senha: Yup.string().min(6, 'Sua deve ter mais de 6 caracteres').required('Senha obrigatória'),
+	senha: Yup.string().min(6, 'Sua senha deve ter no mínimo 6 caracteres').required('Senha obrigatória'),
 	email: Yup.string().email('Insira um email válido').required('Email obrigatório')
 })
