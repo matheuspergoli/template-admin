43494648ba4511d27e5702e79600ad1fa0a2f57e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-14 10:04:31 -0300

    Removendo classes de <Conteudo />

--- components/Conteudo/index.tsx
+++ components/Conteudo/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { IconeUser } from '../../icons'
 import AnimateFadeDiv from '../../motion/AnimateFadeDiv'
 import { UserLoggedContext } from '../../context/UserLoggedContext'
 
@@ -11,7 +10,7 @@ function Conteudo(props: ConteudoProps) {
 	const { user } = React.useContext(UserLoggedContext)
 
 	return (
-		<section className='flex justify-between w-full p-5 bg-gray-300 text-black dark:text-white dark:bg-gray-700'>
+		<section className='w-full p-5 bg-gray-300 text-black dark:text-white dark:bg-gray-700'>
 			<AnimateFadeDiv>{props.children}</AnimateFadeDiv>
 		</section>
 	)
